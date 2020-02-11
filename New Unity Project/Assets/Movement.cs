500a7e03be7bb505f0f7dab8a0e893642145b152
karl eyre <karl_eyre@hotmail.com>
2020-02-11 15:31:06 +1100

    Occulus working in both eyes

---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (OVRInput.)
+        {
+
+        }
+    }
+}
