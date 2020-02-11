52784a72c8723819a88c07b8d424b9f2b3b5a95d
karl eyre <karl_eyre@hotmail.com>
2020-02-11 16:54:59 +1100

    Key inputs
    Forward added

--- New Unity Project/Assets/Movement.cs
+++ New Unity Project/Assets/Movement.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
+    public Text pressText1;
+    public Text pressText2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +18,31 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (OVRInput.)
+        // move forward if button is pressed
+        if (OVRInput.Get(OVRInput.Button.One) == true)
         {
-
+            transform.position += new Vector3(0,0,1);
+            pressText1.text = "You have pressed the button";
         }
+        else
+        {
+            pressText1.text = "You have NOT pressed the button";
+        }
+
+        //if trigger is used reset location
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) == true)
+        {
+            this.transform.position =  new Vector3(0, 5, 0);
+            pressText2.text = "You have pressed the trigger";
+        }
+        else
+        {
+            pressText2.text = "You have NOT pressed the trigger";
+        }
+
+        /*if Input.GetButtonDown(KeyCode.Space)
+         {
+            print(yes);
+         }*/
     }
 }
