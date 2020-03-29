c1578173c9b905a5060e1eb68fde5ced57d45f63
EZG <elliot.gordeno@yahoo.com>
2020-03-30 10:30:57 +1100

    Changing Color Script

--- New Unity Project/Assets/CapColour.cs
+++ New Unity Project/Assets/CapColour.cs
@@ -10,18 +10,18 @@ public class CapColour : MonoBehaviour
     Color GlowIntensity;
     void start()
     {
+        StartCoroutine("PillarCoroutine");
         
-
-    }
-    void Update()
-    {
-
-        Color GlowColor = new Color(0.1f, 0.5f, 0.8f);
+        Color GlowColor = new Color(0.1f, 0.5730338f, 1f);
+        Color BaseColor = new Color(0.0f, 0.2150229f, 0.6698113f);
+        Color GlowIntensity = new Color(0.8f, 0.8f, 0.8f);
+        
         PillarGlow.SetColor("Color_78BD9413", GlowColor);
-        
-        
-        Color GlowIntensity = new Color(0.3f, 0.3f, 0.3f);
         PillarGlow.SetColor("Color_848602D4", GlowIntensity);
+        PillarGlow.SetColor("Color_736EF487", BaseColor);
+        
+       
+        
         
         
         
@@ -29,5 +29,15 @@ public class CapColour : MonoBehaviour
         //This means when changing the color all values have to be the same, IE,
         //public Color(float r, float g, float b);
     }
-
+    void Update()
+    {
+        
+    }
+    
+    IEnumerator PillarCoroutine()
+        {
+            Debug.Log("Started Coroutine at timestape :" + Time.time);
+            yield return new WaitForSeconds(5);
+            Color GlowIntensity = new Color(1f, 1f, 1f);
+        }
 }
