da2d3405cd43718e44f31052ea5a43203fcef540
EZG <elliot.gordeno@yahoo.com>
2020-03-30 09:56:40 +1100

    Color Script Adjustment

--- New Unity Project/Assets/CapColour.cs
+++ New Unity Project/Assets/CapColour.cs
@@ -7,18 +7,27 @@ public class CapColour : MonoBehaviour
 {
     public Material PillarGlow;
     Color GlowColor;
+    Color GlowIntensity;
     void start()
     {
-       
+        
+
     }
     void Update()
     {
-       /// Color GlowColor = new Color(
-       ///         Random.Range(0, 255),
-        ///        Random.Range(0, 255),
-        ///        Random.Range(0, 255));
-       
+
+        Color GlowColor = new Color(0.1f, 0.5f, 0.8f);
         PillarGlow.SetColor("Color_78BD9413", GlowColor);
+        
+        
+        Color GlowIntensity = new Color(0.3f, 0.3f, 0.3f);
+        PillarGlow.SetColor("Color_848602D4", GlowIntensity);
+        
+        
+        
+        //Glow intensity is a greyscale
+        //This means when changing the color all values have to be the same, IE,
+        //public Color(float r, float g, float b);
     }
-    
+
 }
