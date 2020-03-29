063dd4824a30fd642037a1188fc8f5464c015a88
EZG <elliot.gordeno@yahoo.com>
2020-03-29 14:09:05 +1100

    Developing pillar color changer script

---
+++ New Unity Project/Assets/CapColour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Drawing;
+
+public class CapColour : MonoBehaviour
+{
+    public Material PillarGlow;
+    Color GlowColor;
+    void start()
+    {
+       
+    }
+    void Update()
+    {
+       /// Color GlowColor = new Color(
+       ///         Random.Range(0, 255),
+        ///        Random.Range(0, 255),
+        ///        Random.Range(0, 255));
+       
+        PillarGlow.SetColor("Color_78BD9413", GlowColor);
+    }
+    
+}
