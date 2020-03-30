122cbd325b8c286ac76c2013bfd566484b1ed76a
EZG <elliot.gordeno@yahoo.com>
2020-03-30 11:14:56 +1100

    Pillar colour working now

--- New Unity Project/Assets/CapColour.cs
+++ New Unity Project/Assets/CapColour.cs
@@ -8,22 +8,15 @@ public class CapColour : MonoBehaviour
     public Material PillarGlow;
     Color GlowColor;
     Color GlowIntensity;
-    void start()
+    Color BaseColor;
+    
+    void Start()
     {
         StartCoroutine("PillarCoroutine");
         
-        Color GlowColor = new Color(0.1f, 0.5730338f, 1f);
-        Color BaseColor = new Color(0.0f, 0.2150229f, 0.6698113f);
-        Color GlowIntensity = new Color(0.8f, 0.8f, 0.8f);
-        
-        PillarGlow.SetColor("Color_78BD9413", GlowColor);
-        PillarGlow.SetColor("Color_848602D4", GlowIntensity);
-        PillarGlow.SetColor("Color_736EF487", BaseColor);
-        
-       
-        
-        
-        
+        GlowColor = new Color(0.1f, 0.5730338f, 1f);
+        BaseColor = new Color(0.0f, 0.2150229f, 0.6698113f);
+        GlowIntensity = new Color(0.8f, 0.8f, 0.8f);
         
         //Glow intensity is a greyscale
         //This means when changing the color all values have to be the same, IE,
@@ -31,13 +24,17 @@ public class CapColour : MonoBehaviour
     }
     void Update()
     {
-        
+        PillarGlow.SetColor("Color_78BD9413", GlowColor);
+        PillarGlow.SetColor("Color_848602D4", GlowIntensity);
+        PillarGlow.SetColor("Color_736EF487", BaseColor);
     }
     
     IEnumerator PillarCoroutine()
         {
-            Debug.Log("Started Coroutine at timestape :" + Time.time);
-            yield return new WaitForSeconds(5);
-            Color GlowIntensity = new Color(1f, 1f, 1f);
+            
+        yield return new WaitForSeconds(5);
+        Debug.Log("Started Coroutine at timestape :" + Time.time);
+        GlowIntensity = new Color(1f, 1f, 1f);
+        GlowColor = new Color(0.4f, 0.213321f, 0.5643f);
         }
 }
