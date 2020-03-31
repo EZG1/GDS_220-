889c69a92307a1192ca7698608693dc7d445a4e1
Karl Eyre <karl_eyre@hotmail.com>
2020-03-31 15:37:24 +1100

    Clean up of scenes
    maintenance

---
+++ New Unity Project/Assets/MaterialVariants.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialVariants : MonoBehaviour
+{
+    public Color MainColor;
+    public Color SecondaryColor;
+    public int Saturation;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = Instantiate<Material>(GetComponent<Renderer>().material);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetComponent<Renderer>().material.SetColor("Color_78BD9413", MainColor);
+        GetComponent<Renderer>().material.SetColor("Color_78BD9413", SecondaryColor);
+    }
+}
