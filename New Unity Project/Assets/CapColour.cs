d608606f9a352040a93e5773c03949b74ebf3fe0
Karl Eyre <karl_eyre@hotmail.com>
2020-04-07 11:38:54 +1000

    Script updated to allow for build

--- New Unity Project/Assets/CapColour.cs
+++ New Unity Project/Assets/CapColour.cs
@@ -6,17 +6,17 @@ using System.Drawing;
 public class CapColour : MonoBehaviour
 {
     public Material PillarGlow;
-    Color GlowColor;
-    Color GlowIntensity;
-    Color BaseColor;
+    UnityEngine.Color GlowColor;
+    UnityEngine.Color GlowIntensity;
+    UnityEngine.Color BaseColor;
     
     void Start()
     {
         StartCoroutine("PillarCoroutine");
         
-        GlowColor = new Color(0.1f, 0.5730338f, 1f);
-        BaseColor = new Color(0.0f, 0.2150229f, 0.6698113f);
-        GlowIntensity = new Color(0.8f, 0.8f, 0.8f);
+        GlowColor = new UnityEngine.Color(0.1f, 0.5730338f, 1f);
+        BaseColor = new UnityEngine.Color(0.0f, 0.2150229f, 0.6698113f);
+        GlowIntensity = new UnityEngine.Color(0.8f, 0.8f, 0.8f);
         
         //Glow intensity is a greyscale
         //This means when changing the color all values have to be the same, IE,
@@ -34,8 +34,8 @@ public class CapColour : MonoBehaviour
             
         yield return new WaitForSeconds(5);
         Debug.Log("Started Coroutine at timestape :" + Time.time);
-        GlowIntensity = new Color(1f, 1f, 1f);
-        GlowColor = new Color(0.4f, 0.213321f, 0.5643f);
+        GlowIntensity = new UnityEngine.Color(1f, 1f, 1f);
+        GlowColor = new UnityEngine.Color(0.4f, 0.213321f, 0.5643f);
 
         }
 }
