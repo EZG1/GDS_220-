7ba1b18cfa12e501127a3dadd2a9f1af848a55c4
Kyan Rowse <rowse9@gmail.com>
2020-04-13 12:45:09 +1000

    Setup Audio for Davids Test Scene
    -Splash sound
    -Snapshot fade for wave sound with trigger
    -Attenuation Settings 
    -File Optimization

---
+++ New Unity Project/Assets/WaveAudioChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class WaveAudioChange : MonoBehaviour
+{
+    private void Start()
+    {
+       StartCoroutine("StartFade");
+    }
+
+}
