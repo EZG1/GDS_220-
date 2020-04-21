82a93b4223bb1ec3492d98243509a9b81397fa50
Kyan Rowse <rowse9@gmail.com>
2020-04-21 18:19:53 +1000

    Final Audio Pass
    -Added wind howl sound
    -Balanced audio
    -Adjusted attenuation
    -Made pillar Sound 3D
    -Added Mixer Buses for wind howl & pillar Stone sound

---
+++ New Unity Project/Assets/WaveSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSound : MonoBehaviour
+{
+    AudioSource wave;
+    void Start()
+    {
+       wave = GetComponent<AudioSource>();
+    }
+
+    void SoundPlay()
+    {
+        wave.Play();
+    }
+}
